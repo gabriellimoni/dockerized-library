4b4630555a093796475a65675412a3089cf18f4a
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-16 14:33:41 -0300

    wrapping async to catch errors This warpping, let the controllers to handle the events without worring about handling erros

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,18 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import libraryController from '@controllers/library.controller'
 import createLibraryValidator from '@validators/createLibrary.validator'
 
 const router = express.Router()
 
 router
-    .post('/library', createLibraryValidator, libraryController.create)
+    .post('/library', createLibraryValidator, wrapAsync(libraryController.create))
+
+// let error be handled by a middleware,
+// thereby the controller don't need to handle it
+function wrapAsync (fn: Function) {
+    return function (req: Request, res: Response, next: NextFunction) {
+        fn(req, res, next).catch(next)
+    }
+}
 
 export default router
