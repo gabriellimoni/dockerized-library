6aed2f9a1be5e1fb7386da6973e87e211a5361ca
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-16 14:01:47 -0300

    library controller - create library imp

--- src/controllers/library.controller.ts
+++ src/controllers/library.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
+import libraryService from '@services/library.service'
+import Library from '@dto/iLibrary'
 
 const createLibrary = async (req: Request, res: Response) => {
-    return res.send(req.body)
+    const library: Library = req.body
+    await libraryService.createLibrary(library)
+    return res.send(library)
 }
 
 export default {
