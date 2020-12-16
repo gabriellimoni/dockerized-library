33a75837b538bdf9dbacf307c1582a55e0299c67
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-16 18:42:33 -0300

    remove log

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -1,6 +1,5 @@
 import Knex from 'knex'
 import knexConfig from './knexfile_runtime'
-console.log(knexConfig.development)
 import { Model } from 'objection'
 
 const knex = Knex(knexConfig.development)
