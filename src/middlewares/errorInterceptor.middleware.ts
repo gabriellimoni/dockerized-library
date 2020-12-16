0e402b619416da88be53ce59725d48c6b00a56a2
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-16 16:15:03 -0300

    logging errors

--- src/middlewares/errorInterceptor.middleware.ts
+++ src/middlewares/errorInterceptor.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
 import BaseApiError from 'src/errors/BaseApiError'
 
 export default async (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err)
     if (err instanceof BaseApiError) {
         return res.status(err.status).send({
             message: err.message,
