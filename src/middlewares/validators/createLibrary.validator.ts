976a1ef51592a64ed3dc4160168287cea93ee721
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-16 18:43:02 -0300

    removing numeric restrictions from geolocation

--- src/middlewares/validators/createLibrary.validator.ts
+++ src/middlewares/validators/createLibrary.validator.ts
@@ -19,11 +19,9 @@ const _geolocation = body('address.geolocation').exists()
 const latitude = body('address.geolocation.latitude')
     .if(_geolocation)
     .exists().withMessage('Latitude is required when passing geolocation')
-    .isNumeric().withMessage('Latitude must be a number. Eg: -20.090902930920')
 const longitude = body('address.geolocation.longitude')
     .if(_geolocation)
     .exists().withMessage('Longitude is required when passing geolocation')
-    .isNumeric().withMessage('Longitude must be a number. Eg: -10.090902930920')
     
 const validations: Array<ValidationChain> = [
     name,
