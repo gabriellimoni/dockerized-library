ed215c75f460afcd5eec77f00d8186d0bdaf112a
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-18 00:58:16 -0300

    caching libraries

--- src/services/library.service.ts
+++ src/services/library.service.ts
@@ -1,6 +1,7 @@
 import Library from '@dto/iLibrary'
 import LibraryListParams from '@dto/iLibraryListParams'
 import * as libraryRepository from '@repositories/library.repository'
+import * as cacheService from '@services/cache.service'
 
 const createLibrary = async (library: Library) => {
     return libraryRepository.insertLibrary(library)
@@ -11,7 +12,13 @@ const getLibraryById = async (id: number) => {
 }
 
 const listLibraryWithParams = async (params: LibraryListParams) => {
-    return libraryRepository.listLibraryWithParams(params)
+    const cache_key = `libraries_${JSON.stringify(params)}`
+    const cached_data = await cacheService.getCache(cache_key)
+    if (cached_data) return cached_data
+
+    const libraries = await libraryRepository.listLibraryWithParams(params)
+    cacheService.saveCache(cache_key, libraries, 10)
+    return libraries
 }
 
 export default {
