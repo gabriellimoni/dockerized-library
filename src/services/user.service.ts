4137a54bf4a727df9d6dd4d5522a1cbe808c742d
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-18 13:58:58 -0300

    removing useless line

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,6 @@
 import User from "@dto/iUser"
 import { encryptPassword } from '@services/auth.service'
 import * as userRepository from '@repositories/user.repository'
-import EntityNotFoundError from "src/errors/EntityNotFound"
 
 const createUser = async (user: User) => {
     const encryptedPassword = await encryptPassword(user.password as string)
