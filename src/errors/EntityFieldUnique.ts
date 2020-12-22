21d1318212cb0e974b9100b7813952244f3cc75e
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-22 10:32:51 -0300

    fix error class name

--- src/errors/EntityFieldUnique.ts
+++ src/errors/EntityFieldUnique.ts
@@ -1,7 +1,7 @@
 import BaseApiError from "./BaseApiError"
 import { v4 } from 'uuid'
 
-export default class LibraryNameUniqueError extends BaseApiError {
+export default class EntityFieldUniqueError extends BaseApiError {
     constructor (entity: string, field: string, value: string) {
         super(`${entity} with ${field} '${value}' already exists`, 400, v4())
     }
