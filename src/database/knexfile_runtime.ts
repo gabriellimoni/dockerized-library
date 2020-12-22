9ca9fa0cb5e268679d95ead0c98221aa068fb769
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-21 22:55:24 -0300

    fix knexfile deploy

--- src/database/knexfile_runtime.ts
+++ src/database/knexfile_runtime.ts
@@ -1,15 +1,24 @@
 const socketPath = process.env.DB_SOCKET_PATH || '/cloudsql'
 export default {
-    development: {
-      client: 'mysql',
-      connection: {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        socketPath: `${socketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
-      }
-    },
-    debug: true,
-    pool: { min: 2, max: 5 },
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    }
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      socketPath: `${socketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+    }
+  },
+  debug: true,
+  pool: { min: 2, max: 5 },
 }
--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -2,5 +2,9 @@ import Knex from 'knex'
 import knexConfig from './knexfile_runtime'
 import { Model } from 'objection'
 
-const knex = Knex(knexConfig.development)
+const config = process.env.NODE_ENV === 'production'
+    ? knexConfig.production
+    : knexConfig.development
+
+const knex = Knex(config)
 Model.knex(knex)
